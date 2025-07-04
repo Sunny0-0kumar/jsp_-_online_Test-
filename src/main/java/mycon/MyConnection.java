fd46d36c0b6f05e934d5c6637f7c0cf26c2ecda3
Sunny0-0kumar <sunny27012001@gmail.com>
2025-07-04 16:35:11 +0530

    Update MyConnection.java

--- src/main/java/mycon/MyConnection.java
+++ src/main/java/mycon/MyConnection.java
@@ -10,8 +10,12 @@ public class MyConnection {
     public static Connection getconnection(){
         try{
         
-            Class.forName("com.mysql.jdbc.Driver");
-            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/sunny", "root", "Sunny@123");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://yamanote.proxy.rlwy.net:23471/sunny?useSSL=false";
+            String user = "root";
+            String password = "gFBcSIcWsHIaNeUjxDNiUPOoXzrpJBOP";
+
+            con = DriverManager.getConnection(url, user, password);
        
         } catch(Exception ex){
             System.out.println(ex);
