d36ef6557ddbc9df03bfb06c7a9b6a259fd0dc2a
Sunny0-0kumar <sunny27012001@gmail.com>
2025-07-09 23:48:50 +0530

    Update MyConnection.java

--- src/main/java/mycon/MyConnection.java
+++ src/main/java/mycon/MyConnection.java
@@ -11,9 +11,9 @@ public class MyConnection {
         try{
         
             Class.forName("com.mysql.cj.jdbc.Driver");
-            String url = System.getenv(DB_URL);
-            String user = System.getenv(DB_USER);
-            String password = System.getenv(DB_PASSWORD);
+            String url = System.getenv("DB_URL");
+            String user = System.getenv("DB_USER");
+            String password = System.getenv("DB_PASSWORD");
 
             con = DriverManager.getConnection(url, user, password);
        
